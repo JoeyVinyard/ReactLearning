96f804c38ab20e8507748591c5765593c7a9a572
JoeyVinyard <jviny98@yahoo.com>
2017-05-11 00:21:23 -0400

    Finished Learning

--- sampletextgen/src/App.js
+++ sampletextgen/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Output from './Components/Output'
+import Output from './Components/Output';
+import Select from './Components/Controls/Select';
+import Text from './Components/Controls/Text';
 import axios from 'axios';
 
 class App extends Component {
@@ -29,9 +31,29 @@ class App extends Component {
         });
   }
 
+  showHtml(x){
+    this.setState({html: x}, this.getSampleText());
+  }
+
+  changeParas(x){
+    this.setState({paras: x}, this.getSampleText());
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App container">
+        <h1>ReactJS Sample Text Generator</h1>
+        <hr />
+        <form className ="form-inline">
+          <div className="form-group">
+            <label>Include HTML:</label>
+            <Select value={this.state.html} onChange ={this.showHtml.bind(this)} />
+          </div>
+          <div className="form-group">
+            <label>Paragraphs:</label>
+            <Text value={this.state.paras} onChange ={this.changeParas.bind(this)} />
+          </div>
+        </form>
         <Output value={this.state.text}/>
       </div>
     );
